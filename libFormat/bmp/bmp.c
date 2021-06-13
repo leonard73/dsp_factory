54434403f56c5ab6815d8bfd48b232776b320d94
root <panjiaqi54321@qq.com>
2021-06-14 01:31:12 +0800

    add bmp driver

---
+++ libFormat/bmp/bmp.c
@@ -0,0 +1,152 @@
+#include "bmp.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+static unsigned char * bmpFile_memPtr;
+static unsigned char * rawDataPtr;
+void create_bmp_file(   
+                        unsigned char * bmpFileBuf,
+                        unsigned char * p_rawBGR,
+                        unsigned int bitPerPixel,
+                        unsigned int sizeRawData,
+                        unsigned int width,
+                        unsigned int height
+){
+    bitmap_file_header bmpFileHeader;
+    bmpFileHeader.bmpFile_type=0x4D42;
+    bmpFileHeader.bmpFile_size[0]=(BITMAP_HEADER_LENGTH + sizeRawData) & 0xFFFF;
+    bmpFileHeader.bmpFile_size[1]=((BITMAP_HEADER_LENGTH + sizeRawData) >> 16) & 0xFFFF;
+    bmpFileHeader.bmpFile_reversed[0]=0;
+    bmpFileHeader.bmpFile_reversed[1]=0;
+	bmpFileHeader.bmpFile_offbits[0] = (BITMAP_INFO_HEADER_LENGTH) & 0xFFFF;
+	bmpFileHeader.bmpFile_offbits[1] = ((BITMAP_INFO_HEADER_LENGTH) >> 16) & 0xFFFF;
+    bitmap_info_header bmpInfoHeader;
+    bmpInfoHeader.bmpInfo_size=BITMAP_INFO_HEADER_LENGTH;
+    bmpInfoHeader.bmpInfo_width=width;
+    bmpInfoHeader.bmpInfo_height=height;
+    bmpInfoHeader.bmpInfo_planes=1;
+    bmpInfoHeader.bmpInfo_bitcount=bitPerPixel;
+    bmpInfoHeader.bmpInfo_compression=0;
+    bmpInfoHeader.bmpInfo_image_size=sizeRawData;
+    bmpInfoHeader.bmpInfo_xpels_per_meter=0;
+    bmpInfoHeader.bmpInfo_ypels_per_meter=0;
+    bmpInfoHeader.bmpInfo_used_clr=0;
+    bmpInfoHeader.bmpInfo_important_clr=0;
+    memcpy(bmpFileBuf,&bmpFileHeader,sizeof(bmpFileHeader));
+    bmpFileBuf+=sizeof(bmpFileHeader);
+    memcpy(bmpFileBuf,&bmpInfoHeader,sizeof(bmpInfoHeader));
+    bmpFileBuf+=sizeof(bmpInfoHeader);
+    memcpy(bmpFileBuf,p_rawBGR,sizeRawData);
+}
+void cvt_RGB888_BGR888(unsigned char * p_dataRGB888,unsigned int pixelNB)
+{
+    for(int i=0;i<pixelNB*3;i+=3)
+    {
+        unsigned char temp = *(p_dataRGB888+i);
+        *(p_dataRGB888+i) = *(p_dataRGB888+i+2);
+        *(p_dataRGB888+i+2) = temp; 
+    }
+}
+unsigned char * create_bmpFile_rgb888(unsigned char * p_dataRGB888,unsigned int size,unsigned int width,unsigned int height)
+{
+    bmpFile_memPtr=(unsigned char*) malloc(size+BITMAP_HEADER_LENGTH);
+    cvt_RGB888_BGR888( p_dataRGB888,width*height);
+    create_bmp_file(bmpFile_memPtr,p_dataRGB888,24,size,width,height);
+    return bmpFile_memPtr;
+}
+void free_bmpRes()
+{
+    if(bmpFile_memPtr)
+        free(bmpFile_memPtr);
+    if(rawDataPtr)
+        free(rawDataPtr);
+}
+void save_RawRGB_bmpFile(char * savePath,unsigned char * p_dataRGB888,unsigned int size,unsigned int width,unsigned int height)
+{
+    unsigned char * saveBuf = create_bmpFile_rgb888(p_dataRGB888,size,width,height);
+    FILE * fp = fopen(savePath,"wb");
+    fwrite(saveBuf,size+BITMAP_HEADER_LENGTH,sizeof(unsigned char),fp);
+    fclose(fp);
+    free_bmpRes();
+}
+void loadFile(char * loadPath,unsigned int size,unsigned char * buf)
+{
+    FILE * fp = fopen(loadPath,"rb");
+    fread(buf,size,sizeof(unsigned char),fp);
+    fclose(fp);
+}
+void loadBmpFileHeaderPart(char * loadPath,bitmap_file_header * buf)
+{
+    FILE * fp = fopen(loadPath,"rb");
+    fread(buf,1,sizeof(bitmap_file_header),fp);
+    fclose(fp);
+}
+void loadBmpInfoHeaderPart(char * loadPath,bitmap_info_header * buf)
+{
+    FILE * fp = fopen(loadPath,"rb");
+    fseek( fp, BITMAP_FILE_HEADER_LENGTH, SEEK_SET );
+    fread(buf,1,sizeof(bitmap_info_header),fp);
+    fclose(fp);
+}
+void loadBmpDataPart(char * loadPath,unsigned int size,unsigned char * buf)
+{
+    FILE * fp = fopen(loadPath,"rb");
+    fseek( fp, BITMAP_HEADER_LENGTH, SEEK_SET );
+    fread(buf,size,sizeof(unsigned char),fp);
+    fclose(fp);
+}
+unsigned char* read_bmpFile_RawRGB(
+						char * loadPath,
+						unsigned int *bitsPerPixel,
+						unsigned int *size,
+						unsigned int*width,
+						unsigned int*height,
+                        bitmap_file_header *bmpFileHeader,
+                        bitmap_info_header *bmpInfoHeader
+){
+    
+    loadBmpFileHeaderPart(loadPath,bmpFileHeader);
+    loadBmpInfoHeaderPart(loadPath,bmpInfoHeader);
+    *bitsPerPixel   =   (unsigned int)bmpInfoHeader->bmpInfo_bitcount;
+    *width          =   (unsigned int)bmpInfoHeader->bmpInfo_width;
+    *height         =   (unsigned int)bmpInfoHeader->bmpInfo_height;
+    *size           =   (unsigned int)bmpInfoHeader->bmpInfo_image_size;
+    rawDataPtr       =  (unsigned char*)malloc(bmpInfoHeader->bmpInfo_image_size);
+    loadBmpDataPart(loadPath,bmpInfoHeader->bmpInfo_image_size,rawDataPtr);
+    return rawDataPtr;
+}
+void readBMPInfo(char * loadPath)
+{
+    bitmap_file_header bmpFileHeader;
+    bitmap_info_header bmpInfoHeader;
+    unsigned int bitsPerPixel,size,width,height;
+    unsigned char * rgb=read_bmpFile_RawRGB(loadPath,&bitsPerPixel,&size,&width,&height,&bmpFileHeader,&bmpInfoHeader);
+    printf("RawData Size            :   %d Bytes\n",size);
+    printf("RawData bitsPerPixel    :   %d bits/Pixel\n",bitsPerPixel);
+    printf("RawData width           :   %d pixels\n",width);
+    printf("RawData height          :   %d pixels\n",height);
+    printf("RawData compression     :   %d \n",bmpInfoHeader.bmpInfo_compression);
+    printf("RawData bmpInfo_planes  :   %d \n",bmpInfoHeader.bmpInfo_planes);
+    cvt_RGB888_BGR888(rgb,width*height);
+    free_bmpRes();
+}
+//************************test************************************//
+#define test_rgb_width (640)
+#define test_rgb_height (800)
+#define test_save_path "test.bmp"
+#define test_load_path "frame_4394.bin"
+unsigned char testRGB888[test_rgb_width*test_rgb_height*3];
+unsigned char testLoadY8[test_rgb_width*test_rgb_height];
+
+void main()
+{
+    // loadFile(test_load_path,test_rgb_width*test_rgb_height,testLoadY8);
+    // for(int i=0;i<test_rgb_width*test_rgb_height;i++)
+    // {
+    //     testRGB888[i*3]   = testLoadY8[i];
+    //     testRGB888[i*3+1] = testLoadY8[i];
+    //     testRGB888[i*3+2] = testLoadY8[i];
+    // }
+    // save_RawRGB_bmpFile(test_save_path,testRGB888,test_rgb_width*test_rgb_height*3,test_rgb_width,test_rgb_height);
+    readBMPInfo(test_save_path);
+}
---
+++ libFormat/bmp/bmp.h
@@ -0,0 +1,83 @@
+#ifndef __BMP_H__
+#define __BMP_H__
+#define BITMAP_FILE_HEADER_LENGTH (14)
+#define BITMAP_INFO_HEADER_LENGTH (40)
+#define BITMAP_HEADER_LENGTH (BITMAP_FILE_HEADER_LENGTH+BITMAP_INFO_HEADER_LENGTH)
+typedef struct bitmap_file_header_t
+{
+	unsigned short  bmpFile_type;
+	unsigned short  bmpFile_size[2];
+	unsigned short  bmpFile_reversed[2];
+	unsigned short  bmpFile_offbits[2];
+}bitmap_file_header;
+typedef struct bitmap_info_header_t
+{
+	unsigned int    bmpInfo_size;
+	unsigned int    bmpInfo_width;
+	unsigned int    bmpInfo_height;
+	unsigned short  bmpInfo_planes;
+	unsigned short  bmpInfo_bitcount;
+	unsigned int    bmpInfo_compression;
+	unsigned int    bmpInfo_image_size;
+	unsigned int    bmpInfo_xpels_per_meter;
+	unsigned int    bmpInfo_ypels_per_meter;
+	unsigned int    bmpInfo_used_clr;
+	unsigned int    bmpInfo_important_clr;
+}bitmap_info_header;
+void create_bmp_file(   
+                        unsigned char * bmpFileBuf,
+                        unsigned char * p_rawBGR,
+                        unsigned int bitPerPixel,
+                        unsigned int sizeRawData,
+                        unsigned int width,
+                        unsigned int height
+);
+void cvt_RGB888_BGR888( 
+						unsigned char * p_dataRGB888,
+						unsigned int pixelNB
+);
+unsigned char * create_bmpFile_rgb888(
+						unsigned char * p_dataRGB888,
+						unsigned int size,
+						unsigned int width,
+						unsigned int height
+);
+void free_bmpRes();
+void save_RawRGB_bmpFile(
+						char * savePath,
+						unsigned char * p_dataRGB888,
+						unsigned int size,
+						unsigned int width,
+						unsigned int height
+);
+void loadFile(
+						char * loadPath,
+						unsigned int size,
+						unsigned char * buf
+);
+void loadBmpFileHeaderPart(
+						char * loadPath,
+						bitmap_file_header * buf
+);
+void loadBmpInfoHeaderPart(
+						char * loadPath,
+						bitmap_info_header * buf
+);
+void loadBmpDataPart(
+						char * loadPath,
+						unsigned int size,
+						unsigned char * buf
+);
+unsigned char* read_bmpFile_RawRGB(
+						char * loadPath,
+						unsigned int *bitsPerPixel,
+						unsigned int *size,
+						unsigned int*width,
+						unsigned int*height,
+                        bitmap_file_header *bmpFileHeader,
+                        bitmap_info_header *bmpInfoHeader
+);
+void readBMPInfo(
+						char * loadPath
+);
+#endif //__BMP_H__
